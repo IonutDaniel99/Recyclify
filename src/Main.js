d2034910dfa2a83eed54b832c3371d2c3fa94328
Ionut Daniel <ionut.daniel644@gmail.com>
2022-11-02 22:19:07 +0200

    Firebase Install Succesful

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 export default function Main({ navigation }) {
+  GoogleSignin.configure({
+    webClientId: '804203372642-40slgemmask7bfccfqus38anq5h1knpt.apps.googleusercontent.com',
+  })
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true })
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn()
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential)
+  }
+
   const handleScanBarCode = () => {
     console.log('test')
     navigation.navigate('CameraBarcodeScanner')
@@ -16,6 +34,7 @@ export default function Main({ navigation }) {
       <View style={styles.container}>
         <Button title='Scan BarCode' onPress={handleScanBarCode} />
         <Button title='Insert BarCode' onPress={handleInsertBarCode} />
+        <Button title='Google Sign-In' onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))} />
       </View>
     </View>
   )
