ec09ab2ff0a662e26159e833bb1e0144c0d64edc
Ionut Daniel <ionut.daniel644@gmail.com>
2022-11-05 14:22:44 +0200

    Solved

--- src/Main.js
+++ src/Main.js
@@ -5,19 +5,31 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin'
 export default function Main({ navigation }) {
   GoogleSignin.configure({
     webClientId: '804203372642-40slgemmask7bfccfqus38anq5h1knpt.apps.googleusercontent.com',
+    offlineAccess: true,
+    androidClientId: '804203372642-69p82vrqi230nm713d5iqnaks3557ium.apps.googleusercontent.com',
   })
 
   async function onGoogleButtonPress() {
-    // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true })
-    // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn()
-
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken)
-
-    // Sign-in the user with the credential
-    return auth().signInWithCredential(googleCredential)
+    try {
+      await GoogleSignin.hasPlayServices()
+      console.log('reached google sign in')
+      const userInfo = await GoogleSignin.signIn()
+      console.log(userInfo)
+      this.setState({ userInfo })
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('error occured SIGN_IN_CANCELLED')
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('error occured IN_PROGRESS')
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('error occured PLAY_SERVICES_NOT_AVAILABLE')
+      } else {
+        console.log(error)
+        console.log('error occured unknow error')
+      }
+    }
   }
 
   const handleScanBarCode = () => {
@@ -26,7 +38,10 @@ export default function Main({ navigation }) {
   }
 
   const handleInsertBarCode = () => {
-    navigation.navigate('ProductDetailsScreen', { text: 'hello' })
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+    // navigation.navigate('ProductDetailsScreen', { text: 'hello' })
   }
 
   return (
